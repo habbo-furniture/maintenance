0db9d732a751148463835ec280ac5272deea1b40
Daniel Schischkin <daniel@schischkin.info>
2021-09-18 18:40:16 +0200

    Add maintenance page

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,2 +1,10 @@
-<script>import "../app.postcss";</script>
-<slot></slot>
+<script>
+	import '../app.postcss';
+</script>
+
+<header class="pt-2 mb-2">
+	<img width="200px" class="block mx-auto" src="/logo.svg" alt="Habbo.Furniture Logo" />
+</header>
+<main>
+	<slot />
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,1 @@
-<h1>Welcome</h1>
+<h1 class="font-light text-center text-lg text-gray-800">soon.</h1>
